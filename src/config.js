856ae9e1ad76de24031f2638ca8e93835b3d420b
Brian Landry <brian.landry@gmail.com>
2020-12-26 21:15:33 -0800

    Added personal MapBox key

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const MAPBOX_TOKEN = 'pk.eyJ1IjoiYW52YWthIiwiYSI6ImNrNWRqcDdnNTF4MHYzbXAzNDJzODludnYifQ.2WDAbuAzUuchNtCAcZitAw';
+export const MAPBOX_TOKEN = 'pk.eyJ1IjoiYnJpYW5sYW5kcnkiLCJhIjoiY2tqNm5qeHo1MzV4ZzJwbXlvMmdmb3JlaiJ9.Jy-LHR_5Kf3Zhj4YKs4k_w';
